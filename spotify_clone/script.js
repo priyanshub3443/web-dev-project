78a9fa6801e76014bc34365564958c4faf7130b4
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-04-24 20:08:21 +0530

    Update script.js

--- spotify_clone/script.js
+++ spotify_clone/script.js
@@ -1,1 +1,21 @@
 console.log("lest wrte java script")
+
+async function main() {
+
+    let a = await fetch("https://github.com/priyanshub3443/web-dev-project/tree/main/spotify_clone")
+    let response=await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML=response;
+    let as = div.getElementsByTagName("a")
+    let songs=[]
+    for(let index =0; index<as.length;index++){
+        const element=as[index];
+        if(element.href.endsWith(".mp3")){
+            sessionStorage.push(element.href)
+        }
+    }
+return songs
+}
+
+main()
